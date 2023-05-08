87014e8d1c67c3d3c98cc2fa5ef0b85b1430294d
Lianuch <98549639+Lianuch@users.noreply.github.com>
2023-05-08 23:43:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,90 +10,56 @@ class MusicComposition
     public string Artist { get; set; }
     public string Album { get; set; }
     public string Genre { get; set; }
-    public string Duration { get; set; }
+    public TimeSpan Duration { get; set; }
+
+    public MusicComposition(string title, string artist, string album, string genge, TimeSpan duration)
+    {
+        Title = title;
+        Artist = artist;
+        Album = album;
+        Genre = genge;
+        Duration = duration;
+    }
 }
-
-class MusicLibrary
+class Program
 {
-    private List<MusicComposition> compositions = new List<MusicComposition>();
-
-    public void AddComposition(MusicComposition composition)
+    static void Main(string[] args)
     {
-        compositions.Add(composition);
-    }
-
-    public void SortByGenreAndDuration()
-    {
-        compositions.Sort((a, b) =>
+        List<MusicComposition> tracks = new List<MusicComposition>()
         {
-            int genreComparison = a.Genre.CompareTo(b.Genre);
-            if (genreComparison != 0)
-            {
-                return genreComparison;
-            }
-            else
-            {
-                return a.Duration.CompareTo(b.Duration);
-            }
-        });
-    }
-    public List<MusicComposition> GetCompositionsByArtist(string artist)
-    {
-        return compositions.FindAll(Findart => Findart.Artist == artist);
-    }
+            new MusicComposition("Bohemian Rhapsody", "Queen", "A Night at the Opera", "Rock", new TimeSpan(0, 5, 55)),
+            new MusicComposition("This dream", "Stepan Giga", "Without album", "Ballade", new TimeSpan(0, 8, 2)),
+            new MusicComposition("Thriller", "Michael Jackson", "Thriller", "Pop", new TimeSpan(0, 5, 57)),
+            new MusicComposition("Carpathian gold", "Stepan Giga", "The road to the temple", "Civic lyrics", new TimeSpan(0, 4, 53)),
+            new MusicComposition("In heaven", "Sviatoslav Vakarchuk", "Earth", "Rock", new TimeSpan(0, 5, 32))
+        };
+        List<MusicComposition> sortedTracks = tracks
+            .OrderBy(t => t.Genre)
+            .ThenBy(t => t.Duration)
+            .ToList();
 
-    class Program
-    {
-        static void Main(string[] args)
+        Console.WriteLine("Sorted Tracks:");
+        foreach (MusicComposition track in sortedTracks)
         {
-            MusicLibrary library = new MusicLibrary();
-            library.AddComposition(new MusicComposition
-            {
-                Title = "Bohemian Rhapsody",
-                Artist = "Queen",
-                Album = "A Night at the Opera",
-                Genre = "Rock",
-                Duration = "5:54"
-            });
-            library.AddComposition(new MusicComposition
-            {
-                Title = "This dream",
-                Artist = "Stepan Giga",
-                Album = "Without album",
-                Genre = "Ballade",
-                Duration = "3:14"
-            });
-            library.AddComposition(new MusicComposition
-            {
-                Title = "Carpathian gold",
-                Artist = "Stepan Giga",
-                Album = "The road to the temple",
-                Genre = "Civic lyrics",
-                Duration = "4:17"
-            });
-            library.AddComposition(new MusicComposition
-            {
-                Title = "In heaven",
-                Artist = "Sviatoslav Vakarchuk",
-                Album = "Earth",
-                Genre = "Rock",
-                Duration = "3:42"
-
-            });
-            library.SortByGenreAndDuration();
-
-
-            List<MusicComposition> compositionsBySG = library.GetCompositionsByArtist("Stepan Giga");
-            Console.WriteLine("Compositions by Stepan Giga:");
-            foreach (var composition in compositionsBySG)
-            {
-                Console.WriteLine("{0} - {1}", composition.Title, composition.Album);
-            }
-
-
-            Console.ReadKey();
-
-
+            Console.WriteLine($"{track.Title} by {track.Artist} ({track.Genre}, {track.Duration})");
         }
+
+
+        string artistName = "Michael Jackson";
+        List<MusicComposition> tracksByArtist = tracks
+            .Where(t => t.Artist == artistName)
+            .ToList();
+
+        Console.WriteLine($"\nTracks by {artistName}:");
+        foreach (MusicComposition track in tracksByArtist)
+        {
+            Console.WriteLine($"{track.Title} ({track.Album}, {track.Genre}, {track.Duration})");
+        }
+
+
+
+        Console.ReadKey();
+
+
     }
 }
